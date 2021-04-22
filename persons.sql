8a13b50cf10ebe4875b4200343a2db86df52296f
bleachuekiza <bleach_uekiza99@hotmail.com>
2021-04-22 11:09:37 +0700

    DB

---
+++ persons.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `persons_info` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `age` int(10) NOT NULL,
+   PRIMARY KEY (`id`) 
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
